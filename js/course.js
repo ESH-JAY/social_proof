863aff45d3c7ac54c32cfacfe4d0bbea4a2aaaa2
Esley Ageda <esley.ageda@nisit.gov.pg>
2024-11-19 10:01:37 +1000

    commiting the files

---
+++ js/course.js
@@ -0,0 +1,5 @@
+// Enroll button click event
+//also take note that this is temporary till we set our database
+document.getElementById("enroll-btn").addEventListener("click", function() {
+    alert("Thank you for enrolling! We'll send you the course details via email.");
+});
